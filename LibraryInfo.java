da86feb188221bd24985953f524826f4f992844a
shreelathasuranagi <shreelathasuranagi@gmail.com>
2024-10-18 09:13:48 +0530

    Classes and Objects

---
+++ LibraryInfo.java
@@ -0,0 +1,84 @@
+public class LibraryInfo {
+
+    public static void main(String[] args) {
+
+        Library Library1 = new Library();
+
+        Library1.libraryName = "Central Library";
+        Library1.libraryLocation = "Madivala";
+        Library1.libraryPhoneNumber = 9123456789L;
+        Library1.libraryMembershipFee = 500.00;
+        Library1.libraryRating = 9.5f;
+
+        System.out.println("Name of Library: " + Library1.libraryName);
+        System.out.println("Location of Library: " + Library1.libraryLocation);
+        System.out.println("Contact Number of Library: " + Library1.libraryPhoneNumber);
+        System.out.println("Membership Fee: " + Library1.libraryMembershipFee);
+        System.out.println("Library Rating: " + Library1.libraryRating);
+
+        System.out.println("*****************************************************");
+
+        Library Library2 = new Library();
+
+        Library2.libraryName = "City Library";
+        Library2.libraryLocation = "BTM Layout";
+        Library2.libraryPhoneNumber = 9234567890L;
+        Library2.libraryMembershipFee = 300.00;
+        Library2.libraryRating = 8.7f;
+
+        System.out.println("Name of Library: " + Library2.libraryName);
+        System.out.println("Location of Library: " + Library2.libraryLocation);
+        System.out.println("Contact Number of Library: " + Library2.libraryPhoneNumber);
+        System.out.println("Membership Fee: " + Library2.libraryMembershipFee);
+        System.out.println("Library Rating: " + Library2.libraryRating);
+
+        System.out.println("*****************************************************");
+
+        Library Library3 = new Library();
+
+        Library3.libraryName = "Community Library";
+        Library3.libraryLocation = "Koramangala";
+        Library3.libraryPhoneNumber = 9345678901L;
+        Library3.libraryMembershipFee = 100.00;
+        Library3.libraryRating = 7.8f;
+
+        System.out.println("Name of Library: " + Library3.libraryName);
+        System.out.println("Location of Library: " + Library3.libraryLocation);
+        System.out.println("Contact Number of Library: " + Library3.libraryPhoneNumber);
+        System.out.println("Membership Fee: " + Library3.libraryMembershipFee);
+        System.out.println("Library Rating: " + Library3.libraryRating);
+
+        System.out.println("*****************************************************");
+
+        Library Library4 = new Library();
+
+        Library4.libraryName = "Tech Library";
+        Library4.libraryLocation = "HSR Layout";
+        Library4.libraryPhoneNumber = 9456789012L;
+        Library4.libraryMembershipFee = 250.00;
+        Library4.libraryRating = 9.2f;
+
+        System.out.println("Name of Library: " + Library4.libraryName);
+        System.out.println("Location of Library: " + Library4.libraryLocation);
+        System.out.println("Contact Number of Library: " + Library4.libraryPhoneNumber);
+        System.out.println("Membership Fee: " + Library4.libraryMembershipFee);
+        System.out.println("Library Rating: " + Library4.libraryRating);
+
+        System.out.println("*****************************************************");
+
+        Library Library5 = new Library();
+
+        Library5.libraryName = "Classic Library";
+        Library5.libraryLocation = "Indiranagar";
+        Library5.libraryPhoneNumber = 9567890123L;
+        Library5.libraryMembershipFee = 150.00;
+        Library5.libraryRating = 8.0f;
+
+        System.out.println("Name of Library: " + Library5.libraryName);
+        System.out.println("Location of Library: " + Library5.libraryLocation);
+        System.out.println("Contact Number of Library: " + Library5.libraryPhoneNumber);
+        System.out.println("Membership Fee: " + Library5.libraryMembershipFee);
+        System.out.println("Library Rating: " + Library5.libraryRating);
+
+    }
+}
