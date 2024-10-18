bce9a19bef82082f8a924bfe84d2a0cea2dcf4cc
shreelathasuranagi <shreelathasuranagi@gmail.com>
2024-10-18 09:13:16 +0530

    Classes and Objects

---
+++ Library.java
@@ -0,0 +1,29 @@
+public class Library {
+
+    // Attributes of the Library class
+    String libraryName;
+    String libraryLocation;
+    long libraryPhoneNumber;
+    double libraryMembershipFee;
+    float libraryRating;
+
+    // Method to issue a book to a member
+    public static void issueBook() {
+        // Logic to issue a book to a member
+    }
+
+    // Method to return a book
+    public static void returnBook() {
+        // Logic for returning a book
+    }
+
+    // Method to increase the library's book collection
+    public static void increaseCollection() {
+        // Logic to increase the library's collection of books
+    }
+
+    // Method to display the details of the library
+    public static void displayDetails() {
+        // Logic to display library details
+    }
+}
